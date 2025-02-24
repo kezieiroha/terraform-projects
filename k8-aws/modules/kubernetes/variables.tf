d8cf861aab75b6595b465a1af0114c20ecf53857
keziei <40326921+keziei@users.noreply.github.com>
2025-02-24 13:12:18 +0000

    K8

--- k8-aws/modules/kubernetes/variables.tf
+++
@@ -1,6 +0,0 @@
-# ------------------------------------------------------------------------------
-# Module: kubernetes
-# File: modules/kubernetes/variables.tf
-# Author: Kezie Iroha
-# Description: variables for kubernetes module
-# ------------------------------------------------------------------------------
